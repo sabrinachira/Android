73cb6bf1aa944a45a7edbc1b084b796f208e6dd1
sabrinachira <sabrina.chira.14@cnu.edu>
2018-03-18 19:44:36 -0400

    Android pets 3 project. Toolbar, spinner, and settings are in place.

---
+++ app/src/main/java/com/example/sabri/petsproject3/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.sabri.petsproject3;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+
+    }
+
+    /*
+     * onCreateOptionsMenu creates the application's menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent myIntent_action_setting = new Intent(this, SettingsActivity.class);
+        startActivity(myIntent_action_setting);
+        return true;
+    }
+
+
+}
