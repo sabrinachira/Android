0e6edd6b5a1b6ef93a4198a301fae86d6eec0e9d
DillonSykes <dsykes321@gmail.com>
2018-03-20 21:09:44 -0400

    Images show up and change when chosen need to implement the ListPreference Listener so we can determine where Our data is being pulled from.

--- app/src/main/java/com/example/sabri/petsproject3/SettingsActivity.java
+++ app/src/main/java/com/example/sabri/petsproject3/SettingsActivity.java
@@ -3,8 +3,14 @@ package com.example.sabri.petsproject3;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 	@Override
@@ -17,8 +23,12 @@ public class SettingsActivity extends Activity {
 				.beginTransaction();
 		PrefsFragment mPrefsFragment = new PrefsFragment();
 		mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
-		mFragmentTransaction.commit();	    
+		mFragmentTransaction.commit();
+
 	  }
+
+
+
 	
 	public static class PrefsFragment extends PreferenceFragment {
 
@@ -28,6 +38,8 @@ public class SettingsActivity extends Activity {
 
 			// Load the preferences from an XML resource
 			addPreferencesFromResource(R.xml.preferences);
+
 		}
 	}
+
 }
